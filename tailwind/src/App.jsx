e2898427f393a2073f07848313f47841d81d4cdf
RedJyu <piotr.stezycki@protonmai.com>
2023-12-12 15:01:38 +0100

    up

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -1,4 +1,12 @@
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 };
 export default App;
---
+++ tailwind/src/components/Hero.jsx
@@ -0,0 +1,7 @@
+import heroImg from '../assets/hero.svg';
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
+
+const Hero = () => {
+  return <main>Hero</main>;
+};
+export default Hero;

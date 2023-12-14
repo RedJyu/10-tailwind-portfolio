99f451bf514dc6506fbc9a169db585076274481f
RedJyu <piotr.stezycki@protonmai.com>
2023-12-14 14:02:55 +0100

    about

--- tailwind/src/components/About.jsx
+++ tailwind/src/components/About.jsx
@@ -4,7 +4,18 @@ import SectionTitle from './SectionTitle';
 const About = () => {
   return (
     <section className='py-20 align-element' id='about'>
-      <SectionTitle text='about' />
+      <div className='align-element grid md:grid-cols-2 items-center gap-16'>
+        <img src={aboutSvg} alt='about' className='w-full h-64' />
+        <article>
+          <SectionTitle text='about me' />
+          <p className='mt-8 leading-loose'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            vel sapiente voluptas quo maiores quam sint nisi laboriosam? Ad
+            obcaecati in nemo vitae beatae, adipisci impedit harum praesentium
+            temporibus minima.
+          </p>
+        </article>
+      </div>
     </section>
   );
 };

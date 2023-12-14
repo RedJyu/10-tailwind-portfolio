afb7fcf856f4a6ace00ad00334f138d91a9be9f7
RedJyu <piotr.stezycki@protonmai.com>
2023-12-14 12:20:28 +0100

    hero finished

--- tailwind/src/components/Hero.jsx
+++ tailwind/src/components/Hero.jsx
@@ -2,6 +2,37 @@ import heroImg from '../assets/hero.svg';
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 
 const Hero = () => {
-  return <main>Hero</main>;
+  return (
+    <div className='bg-yellow-200 py-24'>
+      <div className='mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8'>
+        <article>
+          <h1 className='text-7xl font-bold tracking-wider'>
+            I'm <span className='text-red-700'>Peter</span>
+          </h1>
+          <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
+            Front-End Developer
+          </p>
+          <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Laudantium, deleniti.
+          </p>
+          <div className='flex gap-x-4 mt-4'>
+            <a href='#'>
+              <FaGithubSquare className='h-8 w-8 hover:text-red-700 duration 300' />
+            </a>
+            <a href='#'>
+              <FaLinkedin className='h-8 w-8 hover:text-red-700 duration 300' />
+            </a>
+            <a href='#'>
+              <FaTwitterSquare className='h-8 w-8 hover:text-red-700 duration 300' />
+            </a>
+          </div>
+        </article>
+        <article className='hidden md:block'>
+          <img src={heroImg} alt='hero' className='h-80 lg:h-96' />
+        </article>
+      </div>
+    </div>
+  );
 };
 export default Hero;

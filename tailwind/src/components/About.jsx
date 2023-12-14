8a1890e04edf26d074264c6fbcd0a5c698e50a00
RedJyu <piotr.stezycki@protonmai.com>
2023-12-14 13:58:40 +0100

    about

---
+++ tailwind/src/components/About.jsx
@@ -0,0 +1,11 @@
+import aboutSvg from '../assets/about.svg';
+import SectionTitle from './SectionTitle';
+
+const About = () => {
+  return (
+    <section className='py-20 align-element' id='about'>
+      <SectionTitle text='about' />
+    </section>
+  );
+};
+export default About;
